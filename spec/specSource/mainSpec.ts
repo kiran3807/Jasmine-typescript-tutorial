1d3ea7643d0f4afa234982921fcd6ecb7560ecbf
Kiran <kiran384707@gmail.com>
2016-08-27 02:34:42 +0530

    got the browser jasmine working using lite-server

--- spec/specSource/mainSpec.ts
+++ spec/specSource/mainSpec.ts
@@ -1,7 +1,10 @@
-/// <reference path="../typings/index.d.ts" />
+/// <reference path="../../typings/index.d.ts" />
 describe("Hello test", function() {
     it("first spec", function() {
       
         expect(1).toBe(2);
     });
 });
+
+let i = 0
+export { i }
